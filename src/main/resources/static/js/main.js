782db3a84360df4728b37a63b4f283cedb82874f
Harshitha Areshetti <areshettiharshitha@gmail.com>
2025-05-28 15:19:49 -0500

    updated changes

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,7 @@
+// src/main/resources/static/js/main.js
+window.onload = function() {
+    alert("Welcome to the Spring Boot Web App!");
+  };
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript loaded and working!");
+});
